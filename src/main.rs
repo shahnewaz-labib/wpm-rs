10a7ef68c9195b901dce14cd15a43bd0934c6a14
shahnewaz-labib <shahnewaz@iut-dhaka.edu>
2024-02-11 17:52:49 +0600

    added quote randomization

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 extern crate ncurses;
 
 use ncurses::*;
+use rand::prelude::*;
+
+// vector with 5 random long quotes
+fn get_quote() -> Vec<&'static str> {
+    vec![
+        "The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.",
+        "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work. And the only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it.",
+        "Your time is limited, don't waste it living someone else's life. Don't be trapped by dogma, which is living the result of other people's thinking. Don't let the noise of other's opinion drowned your own inner voice. And most important, have the courage to follow your heart and intuition, they somehow already know what you truly want to become. Everything else is secondary.",
+        "Don't be trapped by dogma - which is living with the results of other people's thinking. Don't let the noise of others' opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.",
+        "Stay hungry, stay foolish."
+    ]
+}
 
 fn main() {
     initscr();
@@ -17,7 +29,11 @@ fn main() {
 
     let mut quit = false;
     let mut pos = 0;
-    let mut message: &str = "a lazy brown fox jumps over the fence";
+    let mut rng = thread_rng();
+
+    let quotes = get_quote();
+    // get random quote and assign it to message
+    let mut message = quotes[rng.gen_range(0..quotes.len())];
 
     while !quit {
         attron(COLOR_PAIR(1));
@@ -41,7 +57,7 @@ fn main() {
             ch = getch();
             if ch == 32 {
                 clear();
-                message = "a lazy brown fox jumps over the fence";
+                message = quotes[rng.gen_range(0..quotes.len())];
                 quit = false;
                 continue;
             } else if ch == 3 {
